31f26b68fa3a6fa643815dcd4c80c4c3cba5fc38
osoisi <osoisi@gmail.com>
2024-02-15 22:34:14 +0000

    Updated Power BI Dashboards

--- SQLQuery_City_bank_shutdowns.sql
+++ SQLQuery_City_bank_shutdowns.sql
@@ -1,12 +1,11 @@
-CREATE VIEW City_bank_shitdowns_acquisitions AS 
+CREATE VIEW view_city_bank_shutdowns_acquisitions AS 
 SELECT
 fbl.state,
 COUNT(fbl.state) AS Count_of_Shutdowns,
 COUNT(fbl.Acquiring_Bank_ID) AS Acquisitions_per_state,
 COUNT(fbl.state)-COUNT(fbl.Acquiring_Bank_ID) AS Not_acquired_per_state,
 STRING_AGG(fbl.city, ',') AS Cities_of_shutdowns_per_state
-FROM prepared_failed_bank_list as fbl
+FROM prepared_failed_banks as fbl
 LEFT JOIN prepared_list_acquiring_institutions as lai 
 ON fbl.Acquiring_Bank_ID=lai.Acquiring_Bank_ID 
---WHERE fbl.Acquiring_Bank_ID IS NULL
 GROUP BY fbl.state
---
+++ SQLQuery_prep_city_state.sql
@@ -0,0 +1,5 @@
+SELECT 
+      [City]
+      ,[State]
+    INTO city_state
+  FROM [us_banks-database].[dbo].[prepared_failed_banks]
